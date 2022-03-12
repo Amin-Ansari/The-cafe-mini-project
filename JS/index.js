9a226f7f2e7d3a1f7b088d5aa65b60f68edc6b3a
Amin Ansari <devansari@yahoo.com>
2022-03-12 12:13:14 -0800

    The whole project is done

--- JS/index.js
+++ JS/index.js
@@ -8,3 +8,19 @@ function openAndClose() {
     state = false;
   }
 }
+
+function selectTheTitle(theElement) {
+  if (theElement.id === "cold") {
+    document.getElementById("warm").style =
+      "background-color: transparent !important;color: #000;";
+    theElement.style = "background-color: #616161 !important;color: #fff;";
+    document.getElementById("warm-bar").style.display = "none";
+    document.getElementById("cold-bar").style.display = "block";
+  } else {
+    document.getElementById("cold").style =
+      "background-color: transparent !important;color: #000;";
+    theElement.style = "background-color: #616161 !important;color: #fff;";
+    document.getElementById("cold-bar").style.display = "none";
+    document.getElementById("warm-bar").style.display = "block";
+  }
+}
